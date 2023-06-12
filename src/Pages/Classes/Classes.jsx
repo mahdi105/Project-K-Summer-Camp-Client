281b1502459c2219c6588dca55b610b8dcd2358c
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-06-12 09:55:45 +0600

    Loaded classes data from database and completed the classes page

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import Container from '../../components/Utils/Container';
 import SectionHeading from '../../components/Utils/SectionHeading/SectionHeading';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import ClassCard from '../../components/Utils/ClassCard/ClassCard';
 
 const Classes = () => {
+    const {data, isLoading} = useQuery({
+        queryKey: ['classes'],
+        queryFn: async () => {
+            const data = await axios.get('http://localhost:5000/classes');
+            return data
+        }
+    });
+    const classes = !isLoading && data.data ? data.data : [];
     return (
-        <section>
+        <section className='mt-[110px]'>
             <Container>
-                <div className='pb-8'>
+                <div className='pb-4'>
                     <SectionHeading heading='All Classes'></SectionHeading>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-6 mb-16'>
+                    {
+                        classes.map(course => <ClassCard key={course._id} course={course}></ClassCard>)
+                    }
                 </div>
             </Container>
         </section>
